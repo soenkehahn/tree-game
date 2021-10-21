ae2d30d8eb8251df3717cccff407cdbd4318540d
Sönke Hahn <soenkehahn@gmail.com>
2021-10-21 00:06:26 -0400

    Style visual output

--- src/frontend/scene.tsx
+++ src/frontend/scene.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
 import { Phrase } from "./storyGraph";
+import { CSSProperties } from "react";
 
 export function Scene({ phrase }: { phrase: Phrase }) {
   return (
-    <div>
+    <div style={containerStyle}>
       {phrase.map((options, i) => (
         <Options {...options} key={i} />
       ))}
@@ -12,5 +13,30 @@ export function Scene({ phrase }: { phrase: Phrase }) {
 }
 
 function Options({ snippet, focused }: { snippet: string; focused: boolean }) {
-  return <div>{focused ? `[${snippet}]` : ` ${snippet} `} </div>;
+  return (
+    <div style={itemStyle(focused)}>
+      <div style={textStyle}>{snippet}</div>
+    </div>
+  );
 }
+
+const containerStyle: CSSProperties = {
+  display: "flex",
+  alignItems: "stretch",
+  gap: "1rem",
+  margin: "2rem",
+};
+
+const itemStyle = (focused: boolean): CSSProperties => ({
+  flex: "1 1 0",
+  border: `0.3rem solid ${focused ? "black" : "#bbb"}`,
+  borderRadius: "1rem",
+  display: "flex",
+  flexDirection: "column",
+  padding: "0.5rem",
+});
+
+const textStyle: CSSProperties = {
+  fontFamily: "sans-serif",
+  margin: "auto",
+};
