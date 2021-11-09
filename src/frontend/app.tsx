1c016f1f0bb5f80ae6271dcb13f99ce5ec96e53e
Sönke Hahn <soenkehahn@gmail.com>
2021-11-09 13:27:53 -0500

    Rename Phrase type

--- src/frontend/app.tsx
+++ src/frontend/app.tsx
@@ -81,5 +81,5 @@ const Game = ({
     return () => document.removeEventListener(type, callback as any);
   }, []);
 
-  return <Scene phrase={state.graph.currentPhrase()} />;
+  return <Scene phrase={state.graph.toUiValues()} />;
 };
